4f596e57642abb0a5b7b48953175928eb15e2a30
Michael Allemann <michael.allemann@cyberdeck.ch>
2023-05-29 15:28:50 +0200

    wrote at wrong place

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,42 @@ from app import app
 def index():
     return render_template('index.html', title='cyberdeck.ch')
 
+@app.route('/hardware')
+def software():
+    posts = [
+        {
+            'subject': {'tag': 'cyberdeck'},
+            'body': 'PlutoSDR'
+        },
+         {
+            'subject': {'tag': 'cyberdeck'},
+            'body': 'GPD MicroPC'
+        },
+        {
+            'subject': {'tag': 'cyberdeck'},
+            'body': 'Raspberry Pi'
+        },
+        {
+            'subject': {'tag': 'cyberdeck'},
+            'body': 'RTL-SDR'
+        },
+    ]
+    return render_template('hardware.html', title='cyberdeck.ch', posts=posts)
+
 @app.route('/software')
 def software():
     posts = [
         {
             'subject': {'tag': 'cyberdeck'},
             'body': 'pysdr.org'
+        },
+        {
+            'subject': {'tag': 'cyberdeck'},
+            'body': 'gnuradio.org'
+        },
+        {
+            'subject': {'tag': 'cyberdeck'},
+            'body': 'ubuntu-mate.org'
         },
         {
             'subject': {'tag': 'cyberdeck'},
