58b7f8046c34b2687756364abf5f68b2787ba53c
Michael Allemann <michael.allemann@cyberdeck.ch>
2023-05-29 13:12:39 +0200

    first steps

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,22 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World 2.0!"
+    posts = [
+        {
+            'subject': {'tag': 'cyberdeck'},
+            'body': 'Check out pysdr.org'
+        },
+        {
+            'subject': {'tag': 'cyberdeck'},
+            'body': 'Check out blog.miguelgrinberg.com'
+        },
+        {
+            'subject': {'tag': 'cyberdeck'},
+            'body': 'Check out wireguard.com'
+        },
+        {
+            'subject': {'tag': 'cyberdeck'},
+            'body': 'Check out certbot.eff.org'
+        },
+    ]
+    return render_template('index.html', title='cyberdeck.ch', posts=posts)
